00a0008a40778a43991612c98376c3bb26b60bde
akinsola <thomas.lawanson@gmail.com>
2019-12-30 22:02:26 +0000

    updated project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,14 +1,45 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import { PageWrapper } from "./page.styles";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import styled from 'styled-components';
+
+const ProjectWrapper = styled(PageWrapper)`
+  padding: 1em 2em;
+`
+
+const ProjectParagraph = styled.p`
+  color: white;
+`
+
+const ProjectOrderedList = styled.ul`
+  list-style-type: square;
+`
 
 const Project = props => {
+  console.log('2', props.pageContext.content.json);
 
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => <p>{text}</p>,
+    },
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <ProjectParagraph>{children}</ProjectParagraph>,
+      [BLOCKS.OL_LIST]: (node, children) => <ProjectOrderedList> {children} </ProjectOrderedList>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <li> {children} </li>,
+      // [BLOCKS.EMBEDDED_ASSET]: (node, children) => <img src={node.data.target.fields.file['en-US'].url} alt={node.data.target.fields.title['en-US']} />
+    },
+    renderText: text => text.replace('!', '?'),
+  };
+
+  const text = documentToReactComponents(props.pageContext.content.json, options);
   return (
     <Layout>
-      <PageWrapper>
-        <p> Project </p>
-      </PageWrapper>
+      <ProjectWrapper>
+        {text}
+        {/* <p> Project </p> */}
+      </ProjectWrapper>
     </Layout>
   )
 }
