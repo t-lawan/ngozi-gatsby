6aa5a40756954bd6b19aeaaf46b777f9ed462cfc
akinsola <thomas.lawanson@gmail.com>
2020-02-21 18:48:26 +0000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Anja Ngozi`,
+    description: `Anja Ngozi is a London based DJ, whose ecletic sets centre around soulful tunes, lo-fi beats and experimental waves`,
+    author: `Anja Ngozi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
